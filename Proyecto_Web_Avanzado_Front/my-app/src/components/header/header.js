44417a01509175bf151bcd293c784731b4c599a6
MatPerdigues <matperdigues@gmail.com>
2023-05-08 05:55:47 -0300

    V.07mayoII

--- Proyecto_Web_Avanzado_Front/my-app/src/components/header/header.js
+++ Proyecto_Web_Avanzado_Front/my-app/src/components/header/header.js
@@ -1,18 +1,36 @@
 import './header.css';
 import { Fragment } from 'react';
 
-
 export default function Header(){
     return(
         <Fragment>
-            <header id="headerHome">
-                <h1 id="h1Header">AQUARISMA</h1>
-                <ul id="listHeader">
-                    <li>ESTILO DE ACUARIO 1</li>
-                    <li>ESTILO DE ACUARIO 2</li>
-                    <li>ESTILO DE ACUARIO 3</li>
-                </ul>
-            </header>
+
+            
+                <header id="headerHome">
+
+                    <section id="secSuperiorHeader">
+                        
+                        <div id="tituloHeader">
+                            <h1 id="h1Header">AQUARISMA</h1>
+                        </div>
+
+                    </section>
+                    
+                    <section id='secInferior1'>
+
+                        <div id='secInferior1-div1'>
+                            <p id='p1'>ECOSISTEMAS AMAZÓNICOS</p>
+                            <div id='secInferior1-div2'></div>
+                            <div id='secInferior1-div3'></div>
+                        
+                        </div>
+
+                        
+                    </section>
+
+                </header>
+            
+        
         </Fragment>
     )
 }
