e9aedb971ac7fd92132ddd2c7b0ed3ecfb87c2cd
MatPerdigues <matperdigues@gmail.com>
2023-05-12 01:29:08 -0300

    V.11mayo

--- Proyecto_Web_Avanzado_Front/my-app/src/components/header/header.js
+++ Proyecto_Web_Avanzado_Front/my-app/src/components/header/header.js
@@ -1,5 +1,15 @@
 import './header.css';
 import { Fragment } from 'react';
+import img1 from'../images/amazonia.jpg'
+import img2 from '../images/escalar.jpg'
+import img3 from '../images/amazonico.jpg'
+import img4 from '../images/coral.jpg'
+import img5 from '../images/angel.jpg'
+import img6 from '../images/marino.jpg'
+import img7 from '../images/lago.jpg'
+import img8 from '../images/betta.jpg'
+import img9 from '../images/acuario_dulce.jpg'
+
 
 export default function Header(){
     return(
@@ -20,8 +30,42 @@ export default function Header(){
 
                         <div id='secInferior1-div1' class="encabezado">
                             <p id='p1'>ECOSISTEMAS AMAZÓNICOS</p>
-                            <div id='secInferior1-div2' class="menuSecInferior"></div>
+                            <div id='secInferior1-div2' class="menuSecInferior">
+                                <section class="subMenuSecInferior">
+                                    <div>
+                                        <div class="imgSecInferior">
+                                            <img src={img1} alt='img_Amazona'></img>
+                                        </div>
+                                        <p class="pSecInferior" id="pSecInferior1">Visitá los ecosistemas que inspiran a nuestros acuarios</p> 
+                                        
+                                    </div>
+                                    
+                                    <div>
+                                        <div class="imgSecInferior">
+                                            <img src={img2} alt='img_escalar'></img>
+                                        </div>
+                                        <p class="pSecInferior" id="pSecInferior2"> Conocé a los protagonistas de este ecosistema </p>
+                                    </div>
+                                    <div>
+                                        <div class="imgSecInferior">
+                                            <img src={img3} alt='img_amazonico'></img>
+                                        </div>
+                                        <p class="pSecInferior" id="pSecInferior3">Sumergite en nuestros acuarios</p>
+                                    </div>
+                                    
+           
+                                </section>
+
+                                <section class='lineaSecInferior'>
+                                    <div></div>
+                                    <p class="pLineaInferior">Acerca de AQUARISMA</p>
+                                </section>
+ 
+                            </div>
+                            
+                           
                         </div>
+                        
 
                     </section>
 
@@ -29,7 +73,37 @@ export default function Header(){
 
                         <div id='secInferior2-div1' class="encabezado">
                             <p id='p2'>ECOSISTEMAS OCEÁNICOS</p>
-                            <div id='secInferior2-div2' class="menuSecInferior"></div>
+                            <div id='secInferior2-div2' class="menuSecInferior">
+                            <section class="subMenuSecInferior">
+                                    <div>
+                                        <div class="imgSecInferior">
+                                            <img src={img4} alt='img_coral'></img>
+                                        </div>
+                                        <p class="pSecInferior" id="pSecInferior1">Visitá los ecosistemas que inspiran a nuestros acuarios</p> 
+                                        
+                                    </div>
+                                    
+                                    <div>
+                                        <div class="imgSecInferior">
+                                            <img src={img5} alt='img_pez_angel'></img>
+                                        </div>
+                                        <p class="pSecInferior" id="pSecInferior2"> Conocé a los protagonistas de este ecosistema </p>
+                                    </div>
+                                    <div>
+                                        <div class="imgSecInferior">
+                                            <img src={img6} alt='img_acurario_marino'></img>
+                                        </div>
+                                        <p class="pSecInferior" id="pSecInferior3">Sumergite en nuestros acuarios</p>
+                                    </div>
+                                    
+           
+                                </section>
+
+                                <section class='lineaSecInferior'>
+                                    <div></div>
+                                    <p class="pLineaInferior">Acerca de AQUARISMA</p>
+                                </section> 
+                            </div>
                         </div>
 
                     </section>
@@ -38,10 +112,42 @@ export default function Header(){
 
                         <div id='secInferior3-div1' class="encabezado">
                             <p id='p3'>ECOSISTEMAS DE AGUA DULCE</p>
-                            <div id='secInferior3-div2' class="menuSecInferior"></div>
+                            <div id='secInferior3-div2' class="menuSecInferior">
+                            <section class="subMenuSecInferior">
+                                    <div>
+                                        <div class="imgSecInferior">
+                                            <img src={img7} alt='img_lago'></img>
+                                        </div>
+                                        <p class="pSecInferior" id="pSecInferior1">Visitá los ecosistemas que inspiran a nuestros acuarios</p> 
+                                        
+                                    </div>
+                                    
+                                    <div>
+                                        <div class="imgSecInferior">
+                                            <img src={img8} alt='img_betta'></img>
+                                        </div>
+                                        <p class="pSecInferior" id="pSecInferior2"> Conocé a los protagonistas de este ecosistema </p>
+                                    </div>
+                                    <div>
+                                        <div class="imgSecInferior">
+                                            <img src={img9} alt='img_acuario_dulce'></img>
+                                        </div>
+                                        <p class="pSecInferior" id="pSecInferior3">Sumergite en nuestros acuarios</p>
+                                    </div>
+                                    
+           
+                                </section>
+
+                                <section class='lineaSecInferior'>
+                                    <div></div>
+                                    <p class="pLineaInferior">Acerca de AQUARISMA</p>
+                                </section> 
+                            </div>
                         </div>
 
                     </section>
+
+                    
 
                 </header>
             
